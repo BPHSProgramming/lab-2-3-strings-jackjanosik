cd26287d84559d9350f57b936b82fa10ea1cf15f
jackjanosik <jackjanosik@icloud.com>
2025-10-01 13:50:34 -0400

    Add files via upload

--- Lab2.3.py
+++ Lab2.3.py
@@ -6,11 +6,4 @@
 name = input("Enter your full name:\t")
 first_name = name[:name.find(" ",0)]
 last_name = name[(name.find(" ",0))+1:]
-print(f"{first_name.title()}, {last_name.title()}")
-#use 2 seperate print statements for this.
-print(f"{(first_name[:1]).lower()}{(first_name[1:]).upper()}" +\
-      f", {(last_name[:1]).lower()}{(last_name[1:]).upper()}")
-
-#Task 2
-quote = "“Once you start down the dark path, forever will it dominate your destiny."
-encypted_quote = ((quote.upper()).replace())
+print(f"Your name is {first_name} {last_name}")
